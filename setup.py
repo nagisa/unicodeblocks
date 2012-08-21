aea8edc7d77c97d666bb9198df558e242b6357a6
Simonas Kazlauskas <simonas@kazlauskas.me>
2012-08-21 15:03:44 +0300

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import unicodeblocks
+
+setup(
+    name='unicodeblocks',
+    version=unicodeblocks.version,
+    description='Python module for unicode blocks',
+    long_description=read("README.md"),
+    author='Simonas Kazlauskas',
+    license="ISC",
+    url='https://github.com/simukis/unicodeblocks',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Utilities'
+    ],
+    zip_safe=False,
+)
+
--- unicodeblocks.py
+++ unicodeblocks.py
@@ -1,3 +1,16 @@
+# Copyright (c) 2012, Simonas Kazlauskas
+
+# Permission to use, copy,  modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+# INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+# OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+# PERFORMANCE OF THIS SOFTWARE.
 """
 Module, to identify which block character belongs to, what block name is
 and some more convenience objects.
@@ -5,6 +18,9 @@ and some more convenience objects.
 import collections
 import itertools
 __all__ = ['Block', 'Blocks', 'blocks']
+
+version_info = collections.namedtuple('version_info', 'major minor')(0, 1)
+version = "{0.major}.{0.minor}".format(version_info)
 
 class Block(object):
     def __init__(self, name, start, end, *args, **kwargs):
